17da67c9780c31183447ac4fcd1d2fc4309f75d6
AGF117 <156338271+AGF117@users.noreply.github.com>
2026-07-09 08:37:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('searchInput').addEventListener('input',(e)=>{
+  console.log('Buscar:', e.target.value);
+  // Aquí conectaremos la base de datos en la siguiente versión.
+});
