b0898c449ba3884b75f5b47c9b90d5d4e3a81cc3
AGF117 <156338271+AGF117@users.noreply.github.com>
2026-07-09 08:52:24 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('searchInput').addEventListener('input',(e)=>{
-  console.log('Buscar:', e.target.value);
-  // Aquí conectaremos la base de datos en la siguiente versión.
-});
